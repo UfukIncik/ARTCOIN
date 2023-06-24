c13d3ac94b6aee0ddc5c1fb14c8199efc482cf69
Aybars Duran <aybarsduran@outlook.com>
2023-06-24 18:01:41 +0300

    bug fix

--- main.js
+++ main.js
@@ -14,8 +14,10 @@ class Block{
         return SHA256(this.index + this.previousHash + this.timestamp + JSON.stringify(this.data) + this.nonce).toString();
     }
 
+    //proof of work
     mineBlock(difficulty){
         while(this.hash.substring(0, difficulty) !== Array(difficulty + 1).join("0")){
+            this.nonce++;
             this.hash = this.calculateHash();
         }
 
@@ -26,7 +28,7 @@ class Block{
 class Blockchain{
     constructor(){
         this.chain = [this.createGenesisBlock()]; //this.chain  = [this.createGenesisBlock()];
-        this.difficulty = 1;
+        this.difficulty =2;
     }
 
     createGenesisBlock(){
