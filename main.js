b240ab8bded47bfd07e74ca8afb11b5f5feec87a
Bengisu Şahin <74653216+bengisusaahin@users.noreply.github.com>
2023-06-24 17:59:13 +0300

    Added mineBlock method to Block

--- main.js
+++ main.js
@@ -6,18 +6,27 @@ class Block{
         this.timestamp=timestamp;
         this.data=data;
         this.previousHash=previousHash;
-        this.hash = '';
+        this.hash = this.calculateHash();
+        this.nonce = 0;
     }
-
     
     calculateHash(){
-        return SHA256(this.index + this.previousHash + this.timestamp + JSON.stringify(this.data)).toString();
+        return SHA256(this.index + this.previousHash + this.timestamp + JSON.stringify(this.data) + this.nonce).toString();
+    }
+
+    mineBlock(difficulty){
+        while(this.hash.substring(0, difficulty) !== Array(difficulty + 1).join("0")){
+            this.hash = this.calculateHash();
+        }
+
+        console.log("Block mined: " + this.hash);
     }
 }
 
 class Blockchain{
     constructor(){
         this.chain = [this.createGenesisBlock()]; //this.chain  = [this.createGenesisBlock()];
+        this.difficulty = 1;
     }
 
     createGenesisBlock(){
@@ -30,7 +39,7 @@ class Blockchain{
 
     addBlock(newBlock){
         newBlock.previousHash = this.getLatestBlock().hash;
-        newBlock.hash = newBlock.calculateHash();
+        newBlock.mineBlock(this.difficulty);
         this.chain.push(newBlock);
     }
 
@@ -53,15 +62,19 @@ class Blockchain{
 }
 
 let nitroCoin = new Blockchain();
+
+console.log("Mining block 1...");
 nitroCoin.addBlock(new Block(1, "15/01/2023", {amount: 4}));
-nitroCoin.addBlock(new Block(2, "20/01/2023", {amount: 10}));
 
-console.log("Is blockchain valid? " + nitroCoin.isChainValid());
+console.log("Mining block 2...");
+nitroCoin.addBlock(new Block(2, "20/01/2023", {amount: 8}));
 
-nitroCoin.chain[1].data = {amount: 100 };
-nitroCoin.chain[1].hash = nitroCoin.chain[1].calculateHash();
+//console.log("Is blockchain valid? " + nitroCoin.isChainValid());
 
-console.log("Is blockchain valid? " + nitroCoin.isChainValid());
+//nitroCoin.chain[1].data = {amount: 100 };
+//nitroCoin.chain[1].hash = nitroCoin.chain[1].calculateHash();
+
+//console.log("Is blockchain valid? " + nitroCoin.isChainValid());
 
 
 //console.log(JSON.stringify(nitroCoin,null, 4));
