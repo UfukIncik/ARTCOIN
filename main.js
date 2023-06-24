3259325d2afede4add205cc111be1e958709f8ba
Aybars Duran <aybarsduran@outlook.com>
2023-06-24 18:19:19 +0300

    transactions and mining rewards added

--- main.js
+++ main.js
@@ -1,10 +1,17 @@
 const SHA256 = require('crypto-js/sha256');
 
+class Transaction{
+    constructor(fromAddress, toAddress, amount){
+        this.fromAddress = fromAddress;
+        this.toAddress = toAddress;
+        this.amount = amount;
+
+    }
+}
 class Block{
-    constructor(index, timestamp, data, previousHash=''){
-        this.index=index;
+    constructor(timestamp, transactions, previousHash=''){
         this.timestamp=timestamp;
-        this.data=data;
+        this.transactions=transactions;
         this.previousHash=previousHash;
         this.hash = this.calculateHash();
         this.nonce = 0;
@@ -29,20 +36,53 @@ class Blockchain{
     constructor(){
         this.chain = [this.createGenesisBlock()]; //this.chain  = [this.createGenesisBlock()];
         this.difficulty =2;
+        this.pendingTransactions = [];
+        this.miningReward = 100;
     }
 
     createGenesisBlock(){
-        return new Block(0, "01/01/2023","Genesis Block","0");
+        return new Block("01/01/2023","Genesis Block","0");
     }
 
     getLatestBlock(){
         return this.chain[this.chain.length - 1];
     }
 
-    addBlock(newBlock){
-        newBlock.previousHash = this.getLatestBlock().hash;
-        newBlock.mineBlock(this.difficulty);
-        this.chain.push(newBlock);
+    minePendingTransactions(miningRewardAddress){
+        let block = new Block(Date.now(), this.pendingTransactions); //In real world cryptos adding all the pendingTransactions to a block is not possible.
+        block.mineBlock(this.difficulty);
+
+        console.log('Block mined successfully');
+        this.chain.push(block);
+
+        this.pendingTransactions = [
+            new Transaction(null, miningRewardAddress,this.miningReward)
+        ];
+        
+    }
+
+    createTransaction(transaction){
+        this.pendingTransactions.push(transaction);
+    }
+
+    getBalanceOfAddress(address){
+        let balance = 0;
+
+        for(const block of this.chain){
+            for(const trans of block.transactions){
+                if(trans.fromAddress === address){
+                    balance -=trans.amount;
+                }
+                
+                if(trans.toAddress === address ){
+                    balance += trans.amount
+
+                }
+                
+
+            }
+        }
+        return balance;
     }
 
     isChainValid(){
@@ -65,11 +105,28 @@ class Blockchain{
 
 let nitroCoin = new Blockchain();
 
-console.log("Mining block 1...");
-nitroCoin.addBlock(new Block(1, "15/01/2023", {amount: 4}));
+nitroCoin.createTransaction(new Transaction('address1', 'address2 ', 100));
+nitroCoin.createTransaction(new Transaction('address2', 'address1 ', 50));
 
-console.log("Mining block 2...");
-nitroCoin.addBlock(new Block(2, "20/01/2023", {amount: 8}));
+console.log('\n Starting the miner..');
+nitroCoin.minePendingTransactions('aybars-address');
+
+console.log('\nBalance of aybars is', nitroCoin.getBalanceOfAddress('aybars-address'));
+
+console.log('\n Starting the miner agaşin..');
+nitroCoin.minePendingTransactions('aybars-address');
+
+console.log('\nBalance of aybars is', nitroCoin.getBalanceOfAddress('aybars-address'));
+
+
+
+
+
+//console.log("Mining block 1...");
+//nitroCoin.addBlock(new Block(1, "15/01/2023", {amount: 4}));
+
+//console.log("Mining block 2...");
+//nitroCoin.addBlock(new Block(2, "20/01/2023", {amount: 8}));
 
 //console.log("Is blockchain valid? " + nitroCoin.isChainValid());
 
@@ -80,6 +137,7 @@ nitroCoin.addBlock(new Block(2, "20/01/2023", {amount: 8}));
 
 
 //console.log(JSON.stringify(nitroCoin,null, 4));
+
 
 //Each represents a work of art and has the attributes creator, creation_date, and owners. 
 class Artwork {
